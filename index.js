20add9cac32826b8ae11d22f009b73716b294ea3
tanlm <tanlm@54.202.4.43>
2017-07-17 11:32:32 +0700

    add setbasepath to npm module index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let extractLAS2 = require("./source/extractors/las2/las2-extractor");
 let extractASC = require("./source/extractors/ascii/ascii-extractor");
 let extractCSV = require("./source/extractors/csv/csv-extractor");
 
+module.exports.setBasePath = function(path) {
+    extractLAS2.setBasePath(path);
+}
+
 module.exports.extractLAS2 = function (inputURL, options) {
     extractLAS2.extractWell(inputURL, function (result) {
         console.log('Read finished', result);

f4aa7b3da9b0e050ec9efe7778a95f47766f46e7
dodv <dodv212@gmail.com>
2018-06-28 15:10:56 +0700

    update version 1.1.2

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-let extractASC = require("./source/extractors/ascii/ascii-extractor");
-let extractCSV = require("./source/extractors/csv/csv-extractor");
-let decoding = require("./source/extractors/crypto-file/decrypto");
 module.exports.setBasePath = function (path) {
     require('./source/extractors/common-config').dataPath = path;
 };
@@ -11,21 +8,7 @@ module.exports.getBasePath = function (path) {
     return require('./source/extractors/common-config').dataPath;
 };
 
-module.exports.decoding = function (data) {
-    return decoding.decoding(data);
-};
-
-module.exports.extractASC = function (inputURL, callback, options) {
-    extractASC.extractFromASC(inputURL, function (result) {
-        callback(result);
-    }, options);
-};
-
-
 module.exports.LASExtractor = require('./source/extractors/las/las-extractor');
-
-module.exports.extractCSV = function (inputURL, projectId, wellId) {
-    extractCSV.extractFromCSV(inputURL, projectId, wellId);
-};
-
-module.exports.hashDir = require('./source/extractors/hash-dir');;
+module.exports.hashDir = require('./source/extractors/hash-dir');
+module.exports.coreDataExtractor = require('./source/extractors/core_data/core_data-extractor');
+module.exports.asciiExtractor = require('./source/extractors/ascii/ascii-extractor');

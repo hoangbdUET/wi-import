3dce31ff600796bcd69157b7528695efd0851110
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2018-07-18 10:22:56 +0700

    update

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ module.exports.LASExtractor = require('./source/extractors/las/las-extractor');
 module.exports.hashDir = require('./source/extractors/hash-dir');
 module.exports.coreDataExtractor = require('./source/extractors/core_data/core_data-extractor');
 module.exports.asciiExtractor = require('./source/extractors/ascii/ascii-extractor');
+module.exports.extractFromCSV = require('./source/extractors/csv/csv-extractor').extractFromCSV;

97010be4c7649197ab47850f58a611e1486df525
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2019-04-19 09:48:18 +0700

    fix csv-ex

--- source/extractors/csv/csv-extractor.js
+++ source/extractors/csv/csv-extractor.js
@@ -6,39 +6,46 @@ let firstline = require('firstline');
 let hashDir = require('../hash-dir');
 let config = require('../common-config');
 
-function writeFromCsv(buffer, fileName, data, index, defaultNULL) {
-  buffer.count += 0;
-  if (parseFloat(data) == parseFloat(defaultNULL)) {
-    buffer.data += index + ' null' + '\n';
-  } else {
-    buffer.data += index + ' ' + data + '\n';
-  }
-  if (buffer.count >= 1000) {
-    fs.appendFileSync(fileName, buffer.data);
-    buffer.count = 0;
-    buffer.data = '';
-  }
+function writeFromCsv(buffer, fileName, value, index, defaultNull) {
+    let data = "";
+    if (parseFloat(value) === parseFloat(defaultNull)) {
+        data += index + " null" + "\n";
+    }
+    else {
+        data += index + " " + value + "\n";
+    }
+    buffer.writeStream.write(data);
+  // buffer.count += 0;
+  // if (parseFloat(value) == parseFloat(defaultNull)) {
+  //   buffer.data += index + ' null' + '\n';
+  // } else {
+  //   buffer.data += index + ' ' + value + '\n';
+  // }
+  // if (buffer.count >= 1000) {
+  //   fs.appendFileSync(fileName, buffer.data);
+  //   buffer.count = 0;
+  //   buffer.data = '';
+  // }
 }
 
-function customSplit(str, delimiter) {
-  let words;
-  if (str.includes('"')) {
-    str = str.replace(/"[^"]+"/g, function(match, idx, string) {
-      let tmp = match.replace(/"/g, '');
-      return '"' + Buffer.from(tmp).toString('base64') + '"';
-    });
-    words = str.split(delimiter);
-    words = words.map(function(word) {
-      if (word.includes('"')) {
-        return (
-          '"' + Buffer.from(word.replace(/"/g, ''), 'base64').toString() + '"'
-        );
-      } else return word;
-    });
-  } else {
-    words = str.split(delimiter);
-  }
-  return words;
+function customSplit(str, delimiter){
+    let words;
+    if(str.includes('"')){
+        str = str.replace(/"[^"]+"/g, function (match, idx, string){
+            let tmp = match.replace(/"/g, '');
+            return '"' + Buffer.from(tmp).toString('base64') + '"';
+        })
+        words = str.split(delimiter);
+        words = words.map(function(word){
+            if(word.includes('"')){
+                return '"' + Buffer.from(word.replace(/"/g, ''), 'base64').toString() + '"';
+            }
+            else return word;
+        })
+    }else {
+        words = str.split(delimiter);
+    }
+    return words;
 }
 
 function extractFromCSV(inputURL, importData) {
@@ -108,58 +115,56 @@ function extractFromCSV(inputURL, importData) {
           );
           fs.writeFileSync(filePathes[curve.name], '');
           datasets[dataset.name].curves.push(curve);
+			BUFFERS[curve.name] = {
+				writeStream: fs.createWriteStream(filePathes[curve.name])
+			};
         }
       } else {
-        // line = line.split(',');
         line = customSplit(line, ',');
-        fieldsName.forEach(function(fieldName, i) {
-          if (count < 7) {
-            console.log(datasets[wellInfo.dataset].curves[i].type);
-						console.log(datasets[wellInfo.dataset].curves[i].type != 'TEXT')
-          }
-          if (
-            datasets[wellInfo.dataset].curves[i].type != 'TEXT' &&
-            parseFloat(line[i + 1]) != parseFloat(wellInfo.NULL.value)
-          ) {
-            let _format = 'NUMBER';
-            if (isNaN(line[i+1])) {
-              _format = 'TEXT';
-            }
-            datasets[wellInfo.dataset].curves[i].type = _format;
-          }
-          if (importData.coreData) {
-            writeFromCsv(
-              BUFFERS[fieldName],
-              filePathes[fieldName],
-              line[i + 1],
-              line[0],
-              wellInfo.NULL.value
-            );
-          } else {
-            writeFromCsv(
-              BUFFERS[fieldName],
-              filePathes[fieldName],
-              line[i + 1],
-              count - 2,
-              wellInfo.NULL.value
-            );
-          }
-        });
-      }
+		fieldsName.forEach(function(fieldName, i) {
+		  if (
+			datasets[wellInfo.dataset].curves[i].type != 'TEXT' &&
+			parseFloat(line[i + 1]) != parseFloat(wellInfo.NULL.value)
+		  ) {
+			let _format = 'NUMBER';
+			if (isNaN(line[i+1])) {
+			  _format = 'TEXT';
+			}
+			datasets[wellInfo.dataset].curves[i].type = _format;
+		  }
+		  if (importData.coreData) {
+			writeFromCsv(
+			  BUFFERS[fieldName],
+			  filePathes[fieldName],
+			  line[i + 1],
+			  line[0],
+			  wellInfo.NULL.value
+			);
+		  } else {
+			writeFromCsv(
+			  BUFFERS[fieldName],
+			  filePathes[fieldName],
+			  line[i + 1],
+			  count - 2,
+			  wellInfo.NULL.value
+			);
+		  }
+		});
+	  }
       count++;
     });
 
     rl.on('end', function() {
-      console.log(filePathes);
       if (fieldsName) {
         fieldsName.forEach(function(fieldName) {
-          fs.appendFileSync(
-            filePathes[fieldName],
-            BUFFERS[fieldName].data,
-            function(err) {
-              reject('WRONG FORMAT');
-            }
-          );
+			BUFFERS[fieldName].writeStream.end();
+          // fs.appendFileSync(
+          //   filePathes[fieldName],
+          //   BUFFERS[fieldName].data,
+          //   function(err) {
+          //     reject('WRONG FORMAT');
+          //   }
+          // );
         });
       }
 
@@ -177,37 +182,6 @@ function extractFromCSV(inputURL, importData) {
       console.log('Read finished');
       fs.unlinkSync(inputURL);
     });
-    // }
-    // else {
-    //     BUFFERS = "";
-    //     rl.on('line', function (line) {
-    //         line = line.trim();
-    //         let commaPosition = line.indexOf(',');
-    //         let fieldName = line.substring(0, commaPosition);
-    //         line = line.substring(commaPosition + 1, line.length);
-    //         line = line.trim();
-    //         line = line.split(',');
-    //         line.forEach(function (item, index) {
-    //             BUFFERS += index + ' ' + item + '\n';
-    //         });
-    //         filePathes[fieldName] = hashDir.createPath(config.dataPath, importData.userInfo.username + wellInfo.name + curve.datasetname + curve.name + curve.unit + curve.step, curve.name + '.txt');
-    //         fs.writeFileSync(filePathes[fieldName], BUFFERS);
-    //         BUFFERS = "";
-    //     });
-
-    //     rl.on('end', function () {
-    //         console.log('Read finished');
-    //     });
-
-    //     rl.on('error', function (err) {
-    //         if (err) console.log('ExtractCSV has error', err);
-    //         reject(err);
-    //     })
-    //     // }
-    // }, function (err) {
-    //     if (err) console.log(err);
-    //     reject(err);
-    // });
   });
 }
 
